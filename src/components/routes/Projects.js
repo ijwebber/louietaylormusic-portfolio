a8ad9ebc3164b020de19fd64a840394a679031f4
Isaac Webber <isaacjwebber@gmail.com>
2021-05-30 10:25:18 +0100

    added projects

---
+++ src/components/routes/Projects.js
@@ -0,0 +1,13 @@
+import Page from './Page';
+import './Projects.css';
+
+
+export default function Projects(props) {
+    return (
+        <Page position={props.position}>
+            <div className="Projects">
+                This page is coming soon.
+            </div>
+        </Page>
+    );
+}
