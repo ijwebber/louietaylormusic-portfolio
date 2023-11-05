8bbdd6e60ac288431349a196fa7b38747ac44d0f
Joey Groves <link_groves@yahoo.co.uk>
2023-11-05 00:59:20 +0000

    updated footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,6 @@ import './Footer.css';
 
 export default function Footer() {
     return (
-        <div className="Footer">© Louie Taylor Music 2021</div>
+        <div className="Footer">© Louie Taylor Music 2023</div>
     );
 }
